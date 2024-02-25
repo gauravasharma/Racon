84bd7e53725f899adb3e3715038dd062f5eba2db
Gaurava.Sharma <Gaurava.Sharma@envestnet.com>
2024-02-25 13:25:25 -0800

    Layout changes

--- racon/src/pages/Home.jsx
+++ racon/src/pages/Home.jsx
@@ -6,12 +6,13 @@ import homepage from '../Images/homepage.PNG'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import mission from '../Images/mission.jpg'
-import values from '../Images/values.PNG'
+import values from '../Images/values.jpg'
 import culture from '../Images/culture.jpg'
 import Stack from 'react-bootstrap/Stack';
 import hyatt from '../Images/hyatt.png'
 import sheraton from '../Images/sheraton.jpg'
 import mariott from '../Images/mariott.jpg'
+import Image from 'react-bootstrap/Image';
 
 export default function Home()
 {
@@ -19,119 +20,123 @@ export default function Home()
          <>       
           <Container fluid>
             <Row style={{marginTop:'0.5rem'}}>
-            <Col>
-                <Row style={{height:'75%'}}>
-                    <Col><img src={homepage} width="100%" height="82%"></img></Col>
+            <Col xs={8} style={{marginRight: '-1rem'}}>
+                <Row  className='h-40'>
+                    <Col><Image src={homepage} width='100%' height='100%'/></Col>
                 </Row>
-                <Row style={{marginTop:'-6rem' }}>
+                <Row  style={{paddingTop: '0.5rem'}}>
                     <Col>
-                        <Card style={{ width: '26rem' }}>
-                        <Card.Img variant="top" src={mission} height='180rem' />
-                        <Card.Body>
-                            <Card.Title>Misson</Card.Title>
-                            <Card.Text style= {{fontStyle:'italic', fontWeight:'300'}}>
-                             "To identify, analyze and recruit key resources that empower business to achieve their organizational goals, becoming their valued partner in success."
-                            </Card.Text>
-                            <Card.Link href="#">Read more</Card.Link>
-                        </Card.Body>
-                        </Card>
+                        <Stack direction="horizontal" gap={2}>
+                           <div>
+                                <Card style={{borderRadius: '0px',zIndex: '-2' }}>
+                                <Card.Img variant="top" src={mission} height='180rem' style={{borderRadius: '0px'}} />
+                                <Card.Body>
+                                    <Card.Title>Misson</Card.Title>
+                                    <Card.Text style= {{fontStyle:'italic', fontWeight:'300'}}>
+                                    "To identify, analyze and recruit key resources that empower business to achieve their organizational goals, becoming their valued partner in success."
+                                    </Card.Text>
+                                    <Card.Link href="#">Read more</Card.Link>
+                                </Card.Body>
+                                </Card>
+                           </div>
+                           <div>
+                                <Card style={{ borderRadius: '0px',zIndex: '-2' }}>
+                                <Card.Img variant="top" src={values}  height='180rem' style={{borderRadius: '0px'}}/>
+                                <Card.Body>
+                                    <Card.Title>Values</Card.Title>
+                                    <Card.Text style= {{fontStyle:'italic', fontWeight:'300',zIndex: '-1'}}>
+                                    Committed to excellence, integrity, client success and mutual respect, we empower business to achieve their organizational goals.
+                                    </Card.Text>
+                                    <Card.Link href="#">Read more</Card.Link>
+                                </Card.Body>
+                                </Card>
+                           </div>
+                           <div>
+                                <Card style={{borderRadius: '0px' ,zIndex: '-2'}}>
+                                <Card.Img variant="top" src={culture} height='180rem' style={{borderRadius: '0px'}} />
+                                <Card.Body>
+                                    <Card.Title>Culture</Card.Title>
+                                    <Card.Text style= {{fontStyle:'italic', fontWeight:'300'}}>
+                                    Customer-centric approach,collaboration, adaptability, respect, work-life balance, integrity, innovation, celebrating success and open communication
+                                    </Card.Text >
+                                    <Card.Link href="#" style={{color:'rgb(146 158 175)'}}>Read more</Card.Link>
+                                </Card.Body>
+                                </Card>
+                           </div>
+                           
+                        </Stack>
+
                    </Col>
-                   <Col>
-                        <Card style={{ width: '27rem' }}>
-                        <Card.Img variant="top" src={values}  height='180rem'/>
-                        <Card.Body>
-                            <Card.Title>Values</Card.Title>
-                            <Card.Text style= {{fontStyle:'italic', fontWeight:'300'}}>
-                            Committed to excellence, integrity, client success and mutual respect, we empower business to achieve their organizational goals.
-                            </Card.Text>
-                            <Card.Link href="#">Read more</Card.Link>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                   <Col>
-                        <Card style={{ width: '27rem' }}>
-                        <Card.Img variant="top" src={culture} height='180rem'/>
-                        <Card.Body>
-                            <Card.Title>Culture</Card.Title>
-                            <Card.Text style= {{fontStyle:'italic', fontWeight:'300'}}>
-                            Customer-centric approach,collaboration, adaptability, respect, work-life balance, integrity, innovation, celebrating success and open communication
-                            </Card.Text >
-                            <Card.Link href="#">Read more</Card.Link>
-                        </Card.Body>
-                        </Card>
-                   </Col>
+
                 </Row>
             </Col>
-            <Col xs={4}>
+            <Col>
                 <Row style={{marginBottom: '0.5rem'}}>
                     <Col >
-                        <Card style={{ width: '41rem', zIndex: '-1' , backgroundColor:'#0f172a', color: 'white', fontFamily: 'Georgia, Times New Roman, Times, serif'}}>
+                    <Stack>
+                        <div>
+                            <Card style={{  zIndex: '-1' ,borderRadius: '0px', backgroundColor:'#0f172a', color: 'white', fontFamily: 'Georgia, Times New Roman, Times, serif'}}>
+                            <Card.Body>
+                                <Card.Title >Client & Partners</Card.Title>
+                                <Card.Text>
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div >
+                            <Card style={{  zIndex: '-1', borderRadius: '0px'}}>
+                            <Card.Body>
+                                <Card.Text>
+                            <img src={hyatt} height="49rem" width="20%"></img>
+                            <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif', fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div >                       
+                            <Card style={{ zIndex: '-1', borderRadius: '0px'}}>
+                            <Card.Body>
+                                <Card.Text>
+                            <img src={sheraton} height="49rem" width="20%"></img>
+                            <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div>
+                            <Card style={{zIndex: '-1', borderRadius: '0px'}}>
+                            <Card.Body>
+                                <Card.Text>
+                            <img src={mariott} height="49rem" width="20%"></img>
+                            <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div>
+                            <Card style={{ zIndex: '-1',borderRadius: '0px'}}>
+                            <Card.Body>
+                                <Card.Text>
+                            <img src={hyatt} height="49rem" width="20%"></img>
+                            <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100, fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </div>
+                        <div>
+                        <Card style={{zIndex: '-1',borderRadius: '0px'}}>
                         <Card.Body>
-                            <Card.Title >Client & Partners</Card.Title>
                             <Card.Text>
+                          <img src={hyatt} height="49rem" width="20%"></img>
+                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
                             </Card.Text>
+                        </Card.Body>
+                        </Card>
+                        </div>
+                    </Stack>                       
+                   </Col>
+                </Row>
 
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                   </Row>
-                <Row style={{marginBottom: '0.5rem'}}><Col>
-                        <Card style={{ width: '41rem' ,zIndex: '-1'}}>
-                        <Card.Body>
-                            <Card.Text>
-                          <img src={hyatt} height="45rem" width="20%"></img>
-                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif', fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                </Row>
-                <Row style={{marginBottom: '0.5rem'}}>
-                    <Col>
-                        <Card style={{ width: '41rem' ,zIndex: '-1'}}>
-                        <Card.Body>
-                            <Card.Text>
-                          <img src={sheraton} height="45rem" width="20%"></img>
-                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                </Row>
-                <Row style={{marginBottom: '0.5rem'}}><Col>
-                        <Card style={{ width: '41rem' ,zIndex: '-1'}}>
-                        <Card.Body>
-                            <Card.Text>
-                          <img src={mariott} height="45rem" width="20%"></img>
-                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                </Row>
-                <Row style={{marginBottom: '0.5rem'}}><Col>
-                        <Card style={{ width: '41rem' ,zIndex: '-1'}}>
-                        <Card.Body>
-                            <Card.Text>
-                          <img src={hyatt} height="45rem" width="20%"></img>
-                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100, fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
-                            </Card.Text>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                </Row>
-                <Row style={{marginBottom: '0.5rem'}}><Col>
-                        <Card style={{ width: '41rem' ,zIndex: '-1'}}>
-                        <Card.Body>
-                            <Card.Text>
-                          <img src={hyatt} height="45rem" width="20%"></img>
-                          <span style={{float: 'right',fontFamily: 'Georgia, Times New Roman, Times, serif',fontWeight:100,fontStyle: 'Italic'}}>Partner since 2019 and delivering excellence.</span>
-                            </Card.Text>
-                            <Card.Link href="#">View more</Card.Link>
-                        </Card.Body>
-                        </Card>
-                   </Col>
-                </Row>
                
             </Col>
             </Row>         
